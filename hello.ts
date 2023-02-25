fb22d2c3e8f74a21b7dea1d91979b864857a8559
ryu <114303361+ryuapp@users.noreply.github.com>
2023-02-26 01:57:26 +0900

    Fix hono module

--- hello.ts
+++ hello.ts
@@ -1,7 +1,6 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
 import { Hono } from 'https://deno.land/x/hono/mod.ts'
-import { serveStatic } from 'npm:hono/deno'
-import { bearerAuth } from 'https://deno.land/x/hono/middleware.ts'
+import { bearerAuth, serveStatic } from 'https://deno.land/x/hono/middleware.ts'
 
 const app = new Hono()
 const token = 'honoiscool'
