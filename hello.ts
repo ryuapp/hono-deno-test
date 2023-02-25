a4a5339e143d8eebd20b8c25fdb86833e20cbb0d
ryu <114303361+ryuapp@users.noreply.github.com>
2023-02-26 01:44:19 +0900

    Add static file

--- hello.ts
+++ hello.ts
@@ -1,13 +1,17 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
 import { Hono } from 'https://deno.land/x/hono/mod.ts'
+import { serveStatic } from 'npm:hono/deno'
 import { bearerAuth } from 'https://deno.land/x/hono/middleware.ts'
 
 const app = new Hono()
 const token = 'honoiscool'
 
-app.use('/', bearerAuth({ token }))
+app.use('/test.txt', bearerAuth({ token }), serveStatic({ root:"./static" }))
 app.get('/', (c) => c.json({
     message: "Hello Hono!"
+}))
+app.get('/test.txt', (c) => c.json({
+    message: "access ./test.txt!"
 }))
 
 serve(app.fetch)
