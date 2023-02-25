be81cbedb45fb6e391a5c1658bae7b62f9e7869f
ryu <114303361+ryuapp@users.noreply.github.com>
2023-02-26 01:24:13 +0900

    Update module

--- hello.ts
+++ hello.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.167.0/http/server.ts'
+import { serve } from 'https://deno.land/std/http/server.ts'
 import { Hono } from 'https://deno.land/x/hono/mod.ts'
 import { bearerAuth } from 'https://deno.land/x/hono/middleware.ts'
 
