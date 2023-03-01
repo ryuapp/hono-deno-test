67f9391a95b04dc16f6bae5cf47e7839311c7817
ryu <114303361+ryuapp@users.noreply.github.com>
2023-03-02 01:28:28 +0900

    Chore page style
    fix button css

--- dev.tsx
+++ dev.tsx
@@ -1,10 +1,10 @@
-import { serve } from "$http/server.ts";
-import { Hono } from "$hono/mod.ts";
-import { bearerAuth } from "$hono/middleware.ts";
-import { TopPage } from "./pages/index.tsx";
+import { serve } from '$http/server.ts';
+import { Hono } from '$hono/mod.ts';
+import { bearerAuth } from '$hono/middleware.ts';
+import { TopPage } from './pages/index.tsx';
 
 const app = new Hono();
-const token = "honoiscool";
+const token = 'honoiscool';
 async function existFile(filepath: string): Promise<boolean> {
   try {
     await Deno.stat(filepath);
@@ -14,15 +14,15 @@ async function existFile(filepath: string): Promise<boolean> {
   }
 }
 
-app.get("/", (c) => {
-  return c.html(<TopPage />)
+app.get('/', (c) => {
+  return c.html(<TopPage />);
 });
 
-app.use("/api/*", bearerAuth({ token }));
-app.get("/api/armor/get/:name", async (c) => {
-  const name: string = c.req.param("name");
-  const fileName: string = name + ".json";
-  const filePath: string = "./database/" + fileName;
+app.use('/api/*', bearerAuth({ token }));
+app.get('/api/armor/get/:name', async (c) => {
+  const name: string = c.req.param('name');
+  const fileName: string = name + '.json';
+  const filePath: string = './database/' + fileName;
 
   if (!(await existFile(filePath))) {
     return c.json({
@@ -34,19 +34,19 @@ app.get("/api/armor/get/:name", async (c) => {
   Deno.close(file.rid);
 
   return new Response(fileContent, {
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   });
 });
-app.post("/api/armor/create/:name", async (c) => {
-  const name: string = c.req.param("name");
+app.post('/api/armor/create/:name', async (c) => {
+  const name: string = c.req.param('name');
   const encoder = new TextEncoder();
-  const contentBytes = encoder.encode("[\n{\n}\n]");
-  const fileName: string = name + ".json";
-  const filePath: string = "./database/" + fileName;
+  const contentBytes = encoder.encode('[\n{\n}\n]');
+  const fileName: string = name + '.json';
+  const filePath: string = './database/' + fileName;
 
   if (await existFile(filePath)) {
     return c.json({
-      message: fileName + " already exists.",
+      message: fileName + ' already exists.',
     });
   }
   const file = await Deno.open(filePath, { write: true, create: true });
@@ -55,7 +55,7 @@ app.post("/api/armor/create/:name", async (c) => {
   Deno.close(file.rid);
 
   return c.json({
-    message: fileName + " is created.",
+    message: fileName + ' is created.',
   });
 });
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,86 @@
 export const TopPage = () => {
-    return (
-        <>
-        <head>
-            <title>HonoCMS</title>
-            <script src="https://cdn.tailwindcss.com"></script>
-            <script>{readFunc}</script>
-            <script>{createFunc}</script>
-        </head>
+  return (
+    <>
+      <head>
+        <title>HonoCMS</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>{readFunc}</script>
+        <script>{createFunc}</script>
+      </head>
+      <body>
         <Header />
         <main class="mx-5">
-            <h1 class="text-3xl font-bold mb-1">Hello HonoCMS!</h1>
-            <p class="mb-5">This page is work in progress</p>
-            <h2 class="text-2xl font-bold">DataBase</h2>
-            <input id="db-name" type="text" value="example" class="bg-zinc-300" /><br></br>
-            <div>
-                <button class="my-1 mr-2 text-xl border rounded px-1 py-1 transition bg-zinc-200 hover:bg-zinc-400" onClick="readDB()">Read</button>
-                <button class="my-1 text-xl border rounded px-1 py-1 transition bg-zinc-200 hover:bg-zinc-400" onClick="createDB()">Create</button>
-            </div>
-            <p id="database" class="h-10"></p>
+          <h1 class="text-3xl font-bold mb-1">Hello HonoCMS!</h1>
+          <p class="mb-5">This page is work in progress</p>
+          <h2 class="text-2xl font-bold">DataBase Name</h2>
+          <input
+            id="db-name"
+            type="text"
+            value="example"
+            class="bg-zinc-300 p-1 rounded outline-orange-500 mb-2"
+          />
+          <br></br>
+          <div>
+            <button
+              class="my-1 mr-2 text-lg text-white border rounded px-1 py-0.5 transition bg-orange-500  hover:bg-orange-600"
+              onClick="readDB()"
+            >
+              Read
+            </button>
+            <button
+              class="my-1 text-lg text-white border rounded px-1 py-0.5 transition bg-orange-500 hover:bg-orange-600"
+              onClick="createDB()"
+            >
+              Create
+            </button>
+          </div>
+          <p id="database" class="h-10"></p>
         </main>
-        </>
-    );
+      </body>
+    </>
+  );
 };
-export const Header = () =>{
-    return(
-        <header class="bg-orange-500 py-2 mb-5">
-            <div class="text-xl font-bold mx-3 text-white">
-                HonoCMS
-            </div>
-        </header>
-    )
-}
+export const Header = () => {
+  return (
+    <header class="bg-orange-500 py-2 mb-5">
+      <div class="text-xl font-bold mx-3 text-white">HonoCMS</div>
+    </header>
+  );
+};
 
 async function readDB() {
-    const dbName = document.getElementById('db-name') as HTMLInputElement
-    const headersList = {
-        'Accept': '*/*',
-        'Authorization': 'Bearer honoiscool'
-       }
-    const response = await fetch('/api/armor/get/' + dbName?.value, { 
-         method: 'GET',
-         headers: headersList
-    });
-    const jsonData = await response.text();
-    const message = JSON.parse(jsonData).message
-    if(message!== undefined){
-        document.getElementById('database')!.innerHTML = message
-    }else{
-        document.getElementById('database')!.innerHTML = jsonData
-    }
+  const dbName = document.getElementById('db-name') as HTMLInputElement;
+  const headersList = {
+    Accept: '*/*',
+    Authorization: 'Bearer honoiscool',
+  };
+  const response = await fetch('/api/armor/get/' + dbName?.value, {
+    method: 'GET',
+    headers: headersList,
+  });
+  const jsonData = await response.text();
+  const message = JSON.parse(jsonData).message;
+
+  if (message !== undefined) {
+    document.getElementById('database')!.innerHTML = message;
+  } else {
+    document.getElementById('database')!.innerHTML = jsonData;
+  }
 }
 async function createDB() {
-    const dbName = document.getElementById('db-name') as HTMLInputElement
-    const headersList = {
-        'Accept': '*/*',
-        'Authorization': 'Bearer honoiscool'
-       }
-    const response = await fetch('/api/armor/create/' + dbName?.value, { 
-        method: 'POST',
-        headers: headersList
-      });
-    const jsonData = await response.text();
-    const message = JSON.parse(jsonData).message
-    document.getElementById('database')!.innerHTML = message   
+  const dbName = document.getElementById('db-name') as HTMLInputElement;
+  const headersList = {
+    Accept: '*/*',
+    Authorization: 'Bearer honoiscool',
+  };
+  const response = await fetch('/api/armor/create/' + dbName?.value, {
+    method: 'POST',
+    headers: headersList,
+  });
+  const jsonData = await response.text();
+  const message = JSON.parse(jsonData).message;
+
+  document.getElementById('database')!.innerHTML = message;
 }
-const readFunc = '' + readDB
-const createFunc = '' + createDB
+const readFunc = '' + readDB;
+const createFunc = '' + createDB;
